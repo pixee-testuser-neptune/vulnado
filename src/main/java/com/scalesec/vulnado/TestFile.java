7b182062d2e33f7a830beb8101b76467c9ea138a
pixee <help@pixee.ai>
2022-08-10 14:58:23 +0000

    Updated TestFile.java:7 - Hardened this system call to make it resistant to injected commands and commands that target sensitive files Updated TestFile.java:5 - Replaced the weak pseudo-random number generator with a strong one
    [Rules]
     - pixee:java/harden-process-creation
     - pixee:java/secure-random

--- src/main/java/com/scalesec/vulnado/TestFile.java
+++ src/main/java/com/scalesec/vulnado/TestFile.java
@@ -1,9 +1,11 @@
+import io.pixee.security.SystemCommand;
+
 public class TestFile {
   
   public void foo(BufferedReader reader) {
     String a = reader.readLine();
-    String b = reader.readLine() + new Random().nextInt();
+    String b = reader.readLine() + new java.security.SecureRandom().nextInt();
 	  String c = a  + b;
-    Runtime.getRuntime().exec(c);
+    SystemCommand.runCommand(Runtime.getRuntime(), c);
   }
 }
